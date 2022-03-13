6730384009172fd55c031b34a3ddf952bb5d2f40
Brandon Oh <brandonoh2938@gmail.com>
2022-03-12 19:36:34 -0500

    renamed file and split for easier navigation

---
+++ InsertLibraryQuery.sql
@@ -0,0 +1,43 @@
+USE db_library;
+
+INSERT INTO tbl_Library_Branch(
+	BranchName,
+	AddressName
+)VALUES
+	('Sharptown','Baker Street'),
+	('Gary Borrows','Mel Brook Drive'),
+	('Terrace Ville','Any Time Boulvard'),
+	('Jerry BaleTown','Carrer Street'),
+	('Lemon Square','Down Town Place'),
+	('Carrer City','Lime Street')
+;
+
+INSERT INTO tbl_Borrower (
+	Customer,
+	AddressName,
+	PhoneNum
+)VALUES
+	('Emanuel Joyner','Hanover Court','202-918-2132'),
+	('Marius Dickerson','Carriage Lane','239-399-4156'),
+	('Kameron Gough','Gartner Ave','216-594-6223'),
+	('Kathryn Peters','Golden Star Street','432-549-4311'),
+	('Mara Mercado','Golf Ave','240-581-3306'),
+	('Sahib Robins','Sulphur Springs Street','201-865-8906'),
+	('Ella-Mae Wilks','Maple Street','302-708-2422'),
+	('Anne Adkins','Ashley Court','501-461-5660')
+;
+
+INSERT INTO tbl_Publisher (
+	PublisherName,AddressName,PhoneNum
+)VALUES
+	('Rowdy Troutman','Kailee Road','423-364-8527'),
+	('Cruz Grimes','Vivianne Branch','505-644-7134'),
+	('Karina Mata','Gutmann Plains','208-544-9255'),
+	('Benicio Shipp','Batz Tunnel','201-265-2622'),
+	('Jacoby Schulz','Dickens Mill','582-302-3814'),
+	('Giulietta Soper','Ellis Ville','582-300-1624'),
+	('Paulo Bare','Dare Corners','239-275-0225'),
+	('Laylani Anglin','Stroman Overpass','505-644-0666'),
+	('Alonna Seal','Kirlin Station','330-332-8422'),
+	('Makai Rosen','Goodwin Track','208-943-8013')
+;
